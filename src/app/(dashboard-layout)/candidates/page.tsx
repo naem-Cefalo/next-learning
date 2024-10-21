90ef0d3bbdb35a4c392c43024319b5b5652a7081
Naem Bhuiyan <naem.bhuiyan@cefalo.com>
2024-10-21 18:20:54 +0600

    Update page.tsx

--- src/app/(dashboard-layout)/candidates/page.tsx
+++ src/app/(dashboard-layout)/candidates/page.tsx
@@ -3,8 +3,28 @@ import CandidatePage from './_components/CandidatePage';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Candidates',
-  description: '...',
+  openGraph: {
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 function Page() {
