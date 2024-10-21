76669b74d40be3152bce9dc31a23e93bf8629c2b
Naem Bhuiyan <naem.bhuiyan@cefalo.com>
2024-10-21 18:24:17 +0600

    login layout

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -1,3 +1,27 @@
+export const metadata = {
+  openGraph: {
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+
+    locale: 'en_US',
+    type: 'website',
+  },
+};
 export default function LoginLayout({
   children, // will be a page or nested layout
 }: {
